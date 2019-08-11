015f90dc9ef7a73898de03ca2454d03a984b8be7
Neo Ighodaro <neo@creativitykills.co>
2019-08-11 20:03:19 +0200

    Updated the API

--- API/index.js
+++ API/index.js
@@ -16,29 +16,22 @@ let pusher = new Pusher({
   encrypted: true
 });
 
-app.get('/tracks', (req, res) => {
-  res.json(tracks);
-});
+app.get('/tracks', (req, res) => res.json(tracks));
+app.get('/current', (req, res) => res.json(current));
 
-app.get('/current', (req, res) => {
-  res.json(current);
-});
+app.post('tick', (req, res) => {
+  const { id, position, device } = req.body;
 
-app.post('/current', (req, res) => {
   for (let index = 0; index < tracks.data.length; index++) {
-    if (tracks['data'][index]['id'] === parseInt(req.body.ID)) {
+    if (tracks['data'][index]['id'] === parseInt(id)) {
       current = tracks['data'][index];
-      pusher.trigger('spot', 'current', current);
+      pusher.trigger('spot', 'tick', { id, position, device });
+      res.json({ status: true });
       break;
     }
   }
 
-  res.json(current);
-});
-
-app.post('tick', (req, res) => {
-  const { id, pos } = req.body;
-  pusher.trigger('spot', 'tick', { id, pos });
+  res.json({ status: false });
 });
 
 app.listen(3000, () => console.log('Listening on port 3000!'));
